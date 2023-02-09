7dd88782161400b396134480f39b831ff712fc61
elicho <elicho91@kakao.com>
2023-02-09 12:34:27 +0900

    [feat] : 초기설정 세팅

---
+++ src/main/java/com/team8/volunteerworkproject/VolunteerWorkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.team8.volunteerworkproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerWorkProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VolunteerWorkProjectApplication.class, args);
+  }
+
+}
