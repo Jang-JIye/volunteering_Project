8b44ed6db474fcd4f692cb706bc62fc1222cbd3a
Jaewon Lee <ljwon1203@gmail.com>
2023-02-09 16:46:47 +0900

    #17 firstCommit

--- src/main/java/com/team8/volunteerworkproject/service/CommentService.java
+++ src/main/java/com/team8/volunteerworkproject/service/CommentService.java
@@ -2,4 +2,5 @@ package com.team8.volunteerworkproject.service;
 
 public interface CommentService {
 
+  void createComment(CreateCommentRequestDto createCommentRequestDto);
 }
---
+++ src/main/java/com/team8/volunteerworkproject/service/CommentServiceImpl.java
@@ -0,0 +1,6 @@
+package com.team8.volunteerworkproject.service;
+
+public class CommentServiceImpl {
+
+  public void createComment(CreateCommentRequestDto createCommentRequestDto);
+}
